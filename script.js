7bb145137cf53a5f096a0e12ca82a6a0ad32cddc
willmuniz20 <wm.muniz16@gmail.com>
2023-09-21 14:48:45 -0300

    alterações no projeto

---
+++ script.js
@@ -0,0 +1,23 @@
+
+function trocaCor(color){
+
+const circulo=document.querySelector(".circulo")
+
+circulo.style.background= color
+
+}
+   function trocaIphone(img){
+
+const image=document.querySelector(".img-princi")
+image.src=img
+trocaCor(color)
+
+
+   }
+  
+
+
+
+   
+
+
